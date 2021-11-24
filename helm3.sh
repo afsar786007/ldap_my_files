0bd479e81d89642f76cddd515395ba84f51a5316
afsar786007 <91608981+afsar786007@users.noreply.github.com>
2021-11-24 06:54:57 +0530

    Create helm3.sh
    helm install

---
+++ helm3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+
+tar xvf helm-v3.4.1-linux-amd64.tar.gz
+
+sudo mv linux-amd64/helm /usr/local/bin
+
+rm helm-v3.4.1-linux-amd64.tar.gz
+
+rm -rf linux-amd64
+https://linuxhint.com/wp-content/uploads/2018/07/h1.png
