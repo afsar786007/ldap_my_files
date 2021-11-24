73a08d480bfc42bac2a0a415f36b392689747e2e
afsar786007 <91608981+afsar786007@users.noreply.github.com>
2021-11-24 06:58:45 +0530

    Secret.sh

---
+++ secret.sh
@@ -0,0 +1,9 @@
+apiVersion: v1
+type: Opaque
+metadata:
+  name: orchestra-secrets-source
+  namespace: openunison
+data:
+  K8S_DB_SECRET: UTJhQzVNNjBMbWljc3Y0UEphYVhsWlRRbw==
+  unisonKeystorePassword: SGRVSEg1c1Z0ZUdaTjFxdjJEUHlnYk1wZQ==
+kind: Secret
